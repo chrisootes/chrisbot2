032339d2855c7037867c875685cdeb653fb8af81
chrisootes <chris_ootes0101@hotmail.com>
2017-11-19 21:48:44 +0100

    nsfw fix

--- discordbot/cogs/reddit.py
+++ discordbot/cogs/reddit.py
@@ -32,17 +32,27 @@ class RedditCommands:
         """Posts one of top 10 hottest link from the given subreddit."""
         logger.info("reddit command issued by {0}".format(ctx.message.author.name))
 
+        if(reddit_limit>100):
+            await ctx.send("Max limit is 100")
+            return
+
         reddit_top = reddit.subreddit(reddit_sub).hot(limit=reddit_limit)
         reddit_random = math.floor(random.random()*reddit_limit)
+        channel_nsfw = ctx.message.channel.is_nsfw()
 
         logger.info("reddit sub is {0}".format(reddit_sub))
         logger.info("reddit post number is {0}".format(reddit_random))
+        logger.info("channel is nsfw is {0}".format(channel_nsfw))
 
         reddit_counter = 0
         for reddit_post in reddit_top:
             if(reddit_counter == reddit_random):
-                logger.info("reddit post url is {0}".format(reddit_post.url))
-                await ctx.send(reddit_post.url)
+                logger.info("reddit post is nsfw is {0}".format(reddit_post.over_18))
+                if(reddit_post.over_18 == True and channel_nsfw == False):
+                    await ctx.send("This post is nsfw and this channal is not")
+                else:
+                    logger.info("reddit post url is {0}".format(reddit_post.url))
+                    await ctx.send(reddit_post.url)
                 return
             else:
                 reddit_counter += 1
